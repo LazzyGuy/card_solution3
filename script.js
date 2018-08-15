d4819932964deb3d83278ae803d212f926b23cdf
suraj <igenjarus@gmail.com>
2018-08-15 20:42:43 +0530

    animation improved

--- script.js
+++ script.js
@@ -3,21 +3,21 @@ let second = document.getElementById("second");
 let third = document.getElementById("third");
 function a() {
   // first.classList.add("anime");
-  first.classList.add("anime", "back");
-  first.classList.remove("front");
   second.classList.remove("back", "anime");
   second.classList.add("front");
+  first.classList.add("anime", "back");
+  first.classList.remove("front");
 }
 function b() {
   // second.classList.add("anime");
-  second.classList.add("back", "anime");
-  second.classList.remove("front");
   third.classList.remove("back", "anime");
   third.classList.add("front");
+  second.classList.add("back", "anime");
+  second.classList.remove("front");
 }
 function c() {
-  third.classList.add("back", "anime");
-  third.classList.remove("front");
   first.classList.remove("back", "anime");
   first.classList.add("front");
+  third.classList.add("back", "anime");
+  third.classList.remove("front");
 }
